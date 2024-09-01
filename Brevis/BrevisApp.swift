00af4d4cdf9b707ed360ffc00ddb58cfa86d8b18
uwi0 <kakapoweb@gmail.com>
2024-09-01 12:06:03 +0700

    Initial Commit

---
+++ Brevis/BrevisApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrevisApp.swift
+//  Brevis
+//
+//  Created by dwi prasetyo on 01/09/24.
+//
+
+import SwiftUI
+
+@main
+struct BrevisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
